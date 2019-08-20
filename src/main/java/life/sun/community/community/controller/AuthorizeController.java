2b3c4ae3e564cea02020b3b3fc64f0bc22010d20
sunmaio159 <sunmiao159@gmail.com>
2019-08-20 23:10:11 +0800

    授权登录获取用户信息

--- src/main/java/life/sun/community/community/controller/AuthorizeController.java
+++ src/main/java/life/sun/community/community/controller/AuthorizeController.java
@@ -1,14 +1,30 @@
 package life.sun.community.community.controller;
 
+import life.sun.community.community.dto.AccessTokenDTO;
+import life.sun.community.community.dto.GithubUser;
+import life.sun.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.8d87deb123b45fd9");
+        accessTokenDTO.setClient_secret("e3be5dd825995f548780b2e2b6ed447420f5bdbe");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.toString());
         return "index";
     }
 }
